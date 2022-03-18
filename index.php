9cb9553084ed7793784ea37135cd55e2a39d5cb1
Bieldeid <gabrieldeid.android@gmail.com>
2022-03-17 21:58:45 -0300

    finalizada a Introdução ao PHP do curso da DIO

--- index.php
+++ index.php
@@ -1,27 +1,18 @@
-<?php
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formulário de inscrição</title>
+</head>
+<body>
+    <H1>Formulário para Inscrição de Competidores</H1>
 
-$categorias = [];
-$categorias[] = 'infantil';
-$categorias[] = 'adolescente';
-$categorias[] = 'adulto';
-$categorias[] = 'idoso';
-// print_r($categorias);
-
-$nome = 'Deid';
-$idade = 18;
-
-// var_dump($nome);
-// var_dump($idade);
-
-if ($idade >= 6 && $idade <= 12)
-{
-    echo 'infantil';
-} 
-else if ($idade >=13 && $idade <-18) 
-{
-    echo 'adolescente';
-} 
-else
-{
-    echo 'adulto';
-}
+    <form action="script.php" method="post">
+        <p>Seu nome: <input type="text" name="nome" placeholder="Insira seu nome"></p>
+        <p>Sua idade: <input type="text" name="idade" placeholder="Insira sua idade"></p>
+        <p><input type="submit"></p>
+    </form>
+</body>
+</html>
